2f633bf5202e6df3c395d6474ca057e0f781f448
Midhun Jyothis <midhun.jts@gmail.com>
2025-11-14 22:27:38 -0600

    feat(scorer): add MQS scorer entry placeholder

---
+++ packages/scorer/index.ts
@@ -0,0 +1,9 @@
+// MQS scorer entrypoint (placeholder)
+
+export function scoreMessage(_triggers: string[]): any {
+  // TODO: compute MQS + sub-scores from triggers/signals
+  return {
+    mqs: 0,
+    sub_scores: {},
+  };
+}
