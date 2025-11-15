8bbddd604db1467b3c88e200110ac4c73fb134b4
Midhun Jyothis <midhun.jts@gmail.com>
2025-11-14 22:06:04 -0600

    feat(detectors): add detectors entry placeholder

---
+++ packages/detectors/index.ts
@@ -0,0 +1,6 @@
+// Detectors entrypoint (placeholder)
+
+export function runDetectors(_text: string): string[] {
+  // TODO: return list of trigger ids like ["ambiguity", "missing_owner"]
+  return [];
+}
